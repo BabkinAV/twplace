7f3c7011eacb8045fe8d3a330727fa593e5b4c28
Andrey Babkin <babkin.andrey@gmail.com>
2023-04-07 17:49:06 +0300

    drafted layout

---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyledHeader } from './Header.styles'
+
+const Header = () => {
+	return (
+		<StyledHeader>Header component</StyledHeader>
+	)
+}
+
+export default Header
