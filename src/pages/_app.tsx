7691dbc85bc4a6c9e354398ead477ad04c176dc7
Andrey Babkin <babkin.andrey@gmail.com>
2023-04-08 11:27:05 +0300

    added css reset and roboto font globally

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,25 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout/Layout/Layout';
+import { Roboto } from 'next/font/google';
+import '../styles/reset.css'
+
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin', 'cyrillic'],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
