17eb2d8a6199b96dee5f1084e31b842940d6fddd
Andrey Babkin <babkin.andrey@gmail.com>
2023-04-24 16:39:56 +0300

    button hover effect

--- src/components/layout/Header/SearchButton/SearchButton.styles.tsx
+++ src/components/layout/Header/SearchButton/SearchButton.styles.tsx
@@ -3,11 +3,31 @@ import styled from 'styled-components';
 export const StyledSearchButton = styled.button`
   border: #fff;
   color: #000;
+  /* background: linear-gradient(
+    211.73deg,
+    rgba(52, 85, 216, 0.1) -46.71%,
+    rgba(255, 67, 67, 0.1) 121.33%
+  ); */
+
+  font-size: 16px;
+  cursor: pointer;
+
+  -webkit-transition: all 1s ease-out;
+  -moz-transition: all 1s ease-out;
+  -o-transition: all 1s ease-out;
+  transition: all 1s ease-out;
+
   background: linear-gradient(
     211.73deg,
     rgba(52, 85, 216, 0.1) -46.71%,
     rgba(255, 67, 67, 0.1) 121.33%
   );
-  font-size: 16px;
-	cursor: pointer;
+  -webkit-transition: background 0.5s ease-out;
+  -moz-transition: background 0.5s ease-out;
+  -o-transition: background 0.5s ease-out;
+  transition: background 0.5s ease-out;
+
+  &:hover {
+    background: #dba0e2;
+  }
 `;
