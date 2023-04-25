6d896aa4c1ca41b2631ddc3ae0cc7e344b340141
Andrey Babkin <babkin.andrey@gmail.com>
2023-04-25 17:55:29 +0300

    created nav links header pseudo element

--- src/components/Hero/CategoriesNavigaion/CategoriesNavigaion.styles.tsx
+++ src/components/Hero/CategoriesNavigaion/CategoriesNavigaion.styles.tsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import ThreeDotsIcon from '../../assets/images/ThreeDotsIcon.svg';
+
 export const StyledCategoriesNavigaion = styled.nav`
   flex: 1 0 auto;
 
@@ -10,5 +12,16 @@ export const StyledCategoriesNavigaion = styled.nav`
     font-weight: 500;
     font-size: 14px;
     line-height: 16px;
+
+    & a {
+      position: relative;
+    }
+
+    & a::before {
+      content: url(${ThreeDotsIcon.src});
+      position: absolute;
+      left: -19px;
+      top: -1px;
+    }
   }
 `;
--- src/components/Hero/CategoriesNavigaion/CategoriesNavigaion.tsx
+++ src/components/Hero/CategoriesNavigaion/CategoriesNavigaion.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { StyledCategoriesNavigaion } from './CategoriesNavigaion.styles';
+import ThreeDotsIcon from '../../assets/images/ThreeDotsIcon.svg'
+
 
 const CategoriesNavigaion = () => {
   return (
@@ -9,7 +12,7 @@ const CategoriesNavigaion = () => {
         <Link href="#">Категории товаров</Link>
       </div>
 			<div className='nav__body'>
-				
+
 			</div>
     </StyledCategoriesNavigaion>
   );
