78a0f2b05977b62e28fb692e545b4ba2fb6d5331
Andrey Babkin <babkin.andrey@gmail.com>
2023-05-24 17:10:55 +0300

    created checkbox component

--- src/components/UI/Checkbox/Checkbox.styles.tsx
+++ src/components/UI/Checkbox/Checkbox.styles.tsx
@@ -1,3 +1,50 @@
 import styled from 'styled-components';
+import CheckIcon from '../../assets/images/CheckIcon.svg';
 
-export const StyledCheckbox = styled.input``;
+export const StyledCheckbox = styled.div`
+  .checkbox__label {
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    font-size: 11px;
+
+    .checkbox__input {
+      /* Add if not using autoprefixer */
+      -webkit-appearance: none;
+      /* Remove most all native input styles */
+      appearance: none;
+      /* For iOS < 15 */
+      background-color: transparent;
+      /* Not removed via appearance */
+      margin: 0 0 0 40px;
+
+      &::before {
+        content: '';
+        border: 2px solid black;
+        border-radius: 4px;
+        width: 20px;
+        height: 20px;
+        position: absolute;
+        cursor: pointer;
+        top: 0px;
+        left: 0px;
+      }
+
+      &:checked::before {
+        content: url(${CheckIcon.src});
+        border: solid 2px transparent;
+
+        background-image: ${props => props.theme.palette.backgroundGradient},
+          ${props => props.theme.palette.backgroundGradient};
+
+        background-origin: border-box;
+        background-clip: content-box, border-box;
+        box-shadow: 2px 1000px 1px #fff inset;
+				vertical-align: top;
+				padding-bottom: 3px;
+				
+
+      }
+    }
+  }
+`;
--- src/components/UI/Checkbox/Checkbox.tsx
+++ src/components/UI/Checkbox/Checkbox.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import { StyledCheckbox } from './Checkbox.styles';
+// TODO: create checkmark as an Image component, make checked property controllable by state
 
-const Checkbox = () => {
-  return <StyledCheckbox type="checkbox" />;
+const Checkbox = ({
+  checked,
+  handleChange,
+}: {
+  checked?: boolean;
+  handleChange?: React.ChangeEventHandler<HTMLInputElement>;
+}) => {
+  return (
+    <StyledCheckbox>
+      <label className="checkbox__label">
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={handleChange}
+          className="checkbox__input"
+        />
+				Some label
+      </label>
+    </StyledCheckbox>
+  );
 };
 
 export default Checkbox;
